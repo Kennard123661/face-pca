3de6ba34b4380d2cea4c8e4f5cfd55de9a9ef81b
Kennard Ng <Kennard_ng@Outlook.com>
2019-12-22 21:05:16 +0800

    add dataset.py

--- dataset.py
+++ dataset.py
@@ -1,5 +1,52 @@
+import numpy as np
+from torch.utils.data._utils.collate import default_collate
+from torch.utils.data import DataLoader
 
 
+class TrainDataLoader(DataLoader):
+    def __init__(self, data, labels):
+        super(TrainDataLoader, self).__init__()
+        self.x = data
+        self.y = labels
+        assert len(self.x) == len(self.y), 'this should have the same number of samples'
+        assert self.x.shape == 2, 'this should be vectorized.'
+        self.positive_idxs, self.negative_idxs = self.get_positive_negative_idxs()
 
+    def get_positive_negative_idxs(self):
+        positive_idxs, negative_idxs = list(), list()
+        for i, label in enumerate(self.y):
+            positive_idx, negative_idx = list(), list()
 
+            # setup the positive idxs first
+            other_label = self.y[:i]
+            positive_idx += list(np.argwhere(other_label == label))
+            other_label = self.y[i+1:]
+            positive_idx += list(np.argwhere(other_label == label) + i + 1)
+            positive_idxs += positive_idx
 
+            negative_idx = np.setdiff1d(self.y, positive_idxs + [i])
+            negative_idxs += negative_idx
+        return positive_idxs, negative_idxs
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, idx):
+        idx = idx % len(self.x)
+        anchor = self.x[idx]
+        positive_idxs = self.positive_idxs[idx]
+        negative_idxs = self.negative_idxs[idx]
+        positive_idx = np.random.choice(positive_idxs, 1)
+        negative_idx = np.random.choice(negative_idxs, 1)
+
+        positive = self.x[positive_idx]
+        negative = self.x[negative_idx]
+        return anchor, positive, negative
+
+    @staticmethod
+    def collate_fn(batch):
+        anchors, positives, negatives = zip(*list(batch))
+        anchors = default_collate(anchors)
+        positives = default_collate(positives)
+        negatives = default_collate(negatives)
+        return anchors, positives, negatives
